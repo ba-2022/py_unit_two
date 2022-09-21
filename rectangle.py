ec77315fd970a623e98190c70e85b3690dbe6892
ba-2022 <112644336+ba-2022@users.noreply.github.com>
2022-09-21 08:14:06 -0400

    printing problems

--- rectangle.py
+++ rectangle.py
@@ -1,8 +1,13 @@
 # In the space below, create a variable called length and give it a value of 5
+
+length= 5
 print("hello your world")
 # In the space below, create a variable called width and give it a value of 9
-
+width= 9
 # Calculate the area of the rectangle (length times width) and assign the answer to a variable called area.
 
+area= 45
+
 # Print the answer to the user in the form shown on the instruction sheet. Use the variable area as part of your
+print("The area of a rectangle is", area)
 # print statement. Do not "hard code" 45 in your statement.
--- sphere.py
+++ sphere.py
@@ -1,2 +1,11 @@
-# Follow the instructions on the Variables and Expressions sheet to calculate the volume of a sphere with
+# Follow the instructions on the Variables and Expressions sheet to calcul the volume of a sphere with
+radius = 5
+
+pi =  3.14
+
+volume = 523.3333333333334
+
+print("The volume of a sphere is", volume )
+
+
 # a given radius. As a hint, when doing something to the 3rd power, Python has a special operator. r^3 will NOT work.
